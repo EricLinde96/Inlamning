6a15c21b97ba50dc452f0e804a55ac3af5d0202b
Eric Linde <ericlinde19@gmail.com>
2025-10-24 15:00:43 +0200

    La till en kommentar som test

--- GuessingGame.java
+++ GuessingGame.java
@@ -2,5 +2,7 @@ public class GuessingGame {
     public static void main(String[] args) {
         Guesser guess = new Guesser(0, 10);
         guess.start();
+
+// DET HÄR ÄR EN TESTKOMMENTAR
     }
 }
